ae9713556d2b4e3cc2af49517eb6e0fae0239409
PedroMarques <pedrodarochamarquesp@gmail.com>
2025-07-06 23:46:04 -0300

    Sistema de slides feito

--- script.js
+++ script.js
@@ -3,18 +3,32 @@ const modal = document.getElementById('projetos-modal');
 const modalTitle = document.getElementById('modal-title');
 const modalDescription = document.getElementById('modal-description');
 const closeModal = document.querySelector('.close');
-const imagemSaibaMais = document.getElementById('imagem');
+const imagem1 = document.getElementById('imagem1');
+const imagem2 = document.getElementById('imagem2');
+const imagem3 = document.getElementById('imagem3');
+const imagem4 = document.getElementById('imagem4');
+const imagem5 = document.getElementById('imagem5');
+let count = 1;
+document.getElementById("radio1").checked = true;
 
 const projetosDetails = {
   proj1: {
     title: 'Mentes em Jogo',
     description: 'Um tabuleiro com foco na inclusão de crinças de 8 a 12 anos no espectro autista. Esse tabuleiro usa o arduino para mostrar o dado e os desafios que o jogadore deve fazer, além de mostrar onde o jogador está por meio de leds.',
-    imagem:'Downloads/Tabuleiro.jpeg'
+    imagem1:'C:\\Users\\Pedro\\Downloads/Tabuleiro.jpeg',
+    imagem2:'C:\\Users\\Pedro\\Downloads/Tabuleiro.jpeg',
+    imagem3:'C:\\Users\\Pedro\\Downloads/Tabuleiro.jpeg',
+    imagem4:'C:\\Users\\Pedro\\Downloads/Tabuleiro.jpeg',
+    imagem5:'C:\\Users\\Pedro\\Downloads/Tabuleiro.jpeg'
 },
   proj2: {
     title: 'Aplicativo fitness',
-    description: 'Um aplicativo fitness para registro de treinos de crossfit, podendo filtrar treinos e visualizar treinos antigos, além de se poder adicionar metas, foi utilizado o biblioteca "tkinter" do python para fazer a parte gráfica.',
-    imagem:'Downloads/Tabuleiro.jpeg'
+    description: 'Um aplicativo fitness para registro de treinos de crossfit com sistema de cadastro e login, podendo filtrar treinos e visualizar treinos antigos, além de se poder adicionar metas, foi utilizado o biblioteca "tkinter" do python para fazer a parte gráfica.',
+    imagem1:'C:\\Users\\Pedro\\Downloads/novoLoginNovo.jpg',
+    imagem2:'C:\\Users\\Pedro\\Downloads/novoCadastroNovo.jpg',
+    imagem3:'C:\\Users\\Pedro\\Downloads/novoTelaInicial.jpg',
+    imagem4:'C:\\Users\\Pedro\\Downloads/NovoTreinos.jpg',
+    imagem5:'C:\\Users\\Pedro\\Downloads/novoVisualizar.jpg'
   }
 };
 
@@ -23,7 +37,11 @@ projetosButtons.forEach(button => {
     const projetosId = button.getAttribute('data-projetos');
     modalTitle.textContent = projetosDetails[projetosId].title;
     modalDescription.textContent = projetosDetails[projetosId].description;
-    imagemSaibaMais.src  = projetosDetails[projetosId].imagem;
+    imagem1.src  = projetosDetails[projetosId].imagem1;
+    imagem2.src  = projetosDetails[projetosId].imagem2;
+    imagem3.src  = projetosDetails[projetosId].imagem3;
+    imagem4.src  = projetosDetails[projetosId].imagem4;
+    imagem5.src  = projetosDetails[projetosId].imagem5;
     modal.style.display = 'grid';
   });
 });
@@ -37,3 +55,17 @@ window.addEventListener('click', (event) => {
     modal.style.display = 'none';
   }
 });
+
+setInterval(function(){
+  nextImage();
+},2000);
+
+function nextImage(){
+  count++;
+
+  if(count > 5){
+    count = 1;
+  }
+
+  document.getElementById("radio"+count).checked = true;
+}
